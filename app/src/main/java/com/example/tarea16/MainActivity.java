1d8f06803394700c897473dd3e8d7e3e56ffcd23
David Asencios <asenciosdavid68@gmail.com>
2026-07-09 14:13:31 -0500

    Add logout option to navigation drawer

--- app/src/main/java/com/example/tarea16/MainActivity.java
+++ app/src/main/java/com/example/tarea16/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tarea16;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.tarea16.activities.LoginActivity;
+import com.example.tarea16.api.TokenManager;
 import com.example.tarea16.databinding.ActivityMainBinding;
 import com.example.tarea16.sync.SyncManager;
 import com.google.android.material.navigation.NavigationView;
@@ -29,7 +32,26 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_logout) {
+                cerrarSesion();
+                return true;
+            }
+            boolean handled = NavigationUI.onNavDestinationSelected(item, navController);
+            if (handled) {
+                drawer.close();
+            }
+            return handled;
+        });
         new SyncManager(this).sincronizar(null);
+    }
+
+    private void cerrarSesion() {
+        new TokenManager(this).limpiar();
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     @Override
